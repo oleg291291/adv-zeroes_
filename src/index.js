c5139ec0471c91c15fcf178d6071f42d40d647f5
oleg291291@mail.ru <oleg291291@mail.ru>
2018-02-22 17:23:17 +0300

    околодесятеричное

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+
+var halfBase;
+if(base % 2 == 0){
+  halfBase = base / 2;
+}
+else{
+  halfBase = base;
+}
+
+  var result = Math.floor(number / halfBase) + Math.floor(number / (halfBase * halfBase)) + Math.floor(number / (halfBase * halfBase * halfBase)) + Math.floor(number / (halfBase * halfBase * halfBase * halfBase)) + Math.floor(number / (halfBase * halfBase * halfBase * halfBase * halfBase)) + Math.floor(number / (halfBase * halfBase * halfBase * halfBase * halfBase * halfBase)) + Math.floor(number / (halfBase * halfBase * halfBase * halfBase * halfBase * halfBase * halfBase));
+return Math.floor(result);
+
 }
