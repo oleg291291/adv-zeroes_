a84659ef2400b49bd5b97e5b8426dc5d094e1a61
oleg291291@mail.ru <oleg291291@mail.ru>
2018-02-22 19:20:16 +0300

    more accurete and go!

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 module.exports = function getZerosCount(number, base) {
 //hm
 
-var result4;
-var eightBase;
-if(base % 8 == 0){
-  console.log('na 8');
-  eightBase = base / 8;
-  result4 = Math.floor(number / eightBase) + Math.floor(number / (eightBase * eightBase)) + Math.floor(number / (eightBase * eightBase* eightBase)) + Math.floor(number / (eightBase * eightBase * eightBase * eightBase)) + Math.floor(number / (eightBase * eightBase * eightBase * eightBase * eightBase));
-  return result4;
+var resultNa7;
+var sevenBase;
+if(base % 12 == 0){
+  
+  sevenBase = base / 12;
+  resultNa7 = Math.floor(number / sevenBase) + Math.floor(number / (sevenBase * sevenBase)) + Math.floor(number / (sevenBase * sevenBase* sevenBase)) + Math.floor(number / (sevenBase * sevenBase * sevenBase * sevenBase)) + Math.floor(number / (sevenBase * sevenBase * sevenBase * sevenBase * sevenBase));
+  return resultNa7;
 }
 else{
-  result4 = 0;
+  resultNa7 = 0;
 }
+
+var resultNa8;
+var eightBase;
+if(base % 8 == 0){
+  
+  eightBase = base / 8;
+  resultNa8 = Math.floor(number / eightBase) + Math.floor(number / (eightBase * eightBase)) + Math.floor(number / (eightBase * eightBase* eightBase)) + Math.floor(number / (eightBase * eightBase * eightBase * eightBase)) + Math.floor(number / (eightBase * eightBase * eightBase * eightBase * eightBase));
+  return resultNa8;
+}
+else{
+  resultNa8 = 0;
+}
+
+
+
+
 
 
 
@@ -26,30 +42,30 @@ else{
   var result = Math.floor(number / halfBase) + Math.floor(number / (halfBase * halfBase)) + Math.floor(number / (halfBase * halfBase * halfBase)) + Math.floor(number / (halfBase * halfBase * halfBase * halfBase)) + Math.floor(number / (halfBase * halfBase * halfBase * halfBase * halfBase)) + Math.floor(number / (halfBase * halfBase * halfBase * halfBase * halfBase * halfBase)) + Math.floor(number / (halfBase * halfBase * halfBase * halfBase * halfBase * halfBase * halfBase));
 
 
-var result2;
-var trBase;
-if(base % 3 == 0){
-  console.log('na 3');
-  trBase = base / 3;
-  result2 = Math.floor(number / trBase) + Math.floor(number / (trBase * trBase))
+// var result2;
+// var trBase;
+// if(base % 3 == 0){
+//   console.log('na 3');
+//   trBase = base / 3;
+//   result2 = Math.floor(number / trBase) + Math.floor(number / (trBase * trBase))
 
-}
-else{
-  result2 = 0;
-}
+// }
+// else{
+//   result2 = 0;
+// }
 
-var result3;
-var quartBase;
-if(base % 4 == 0){
-  console.log('na 4');
-  quartBase = base / 4;
-  result3 = Math.floor(number / quartBase) + Math.floor(number / (quartBase * quartBase));
-}
-else{
-  result3 = 0;
-}
+// var result3;
+// var quartBase;
+// if(base % 4 == 0){
+//   console.log('na 4');
+//   quartBase = base / 4;
+//   result3 = Math.floor(number / quartBase) + Math.floor(number / (quartBase * quartBase));
+// }
+// else{
+//   result3 = 0;
+// }
 
 
-return (result + result2 + result3);
+return (result);
 
 }
